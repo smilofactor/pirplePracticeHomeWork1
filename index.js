4c38a6fc246c2c6f85dabd7ae4dd3b036fee77d5
Steve M <smilovidov@gmail.com>
2018-10-04 00:06:40 -0400

    Created index.js, testing server connection

---
+++ index.js
@@ -0,0 +1,22 @@
+
+/*
+ *
+ * Primary file for the API
+ * index.js
+ *
+ */
+
+//Dependencies
+var http = require('http');
+
+
+//The server should respond to all requests with a string
+var server = http.createServer(function(req,res) {
+  res.end('Outputting info\n');
+});
+
+
+//Start the server, liste on port 3000
+server.listen(3000,function(){
+  console.log("The server is listening on port 3000");
+});
