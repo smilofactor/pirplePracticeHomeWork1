bc91459bd19b6b218322708395debbf2c7d7ec3d
Steve M <smilovidov@gmail.com>
2018-10-04 07:12:28 -0400

    Added code to capture URL path from HTTP request.Sec3,Vid4

--- index.js
+++ index.js
@@ -7,11 +7,28 @@
 
 //Dependencies
 var http = require('http');
+var url = require('url');
 
 
 //The server should respond to all requests with a string
 var server = http.createServer(function(req,res) {
+
+  //Get URL and parse it
+  var parsedUrl = url.parse(req.url,true);
+
+  //Get path
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g,'');
+  
+ 
+  //Send response
   res.end('Outputting info\n');
+
+
+  //Log the request path
+  console.log('Request received on path: '+trimmedPath);
+
+
 });
 
 
@@ -19,3 +36,5 @@ var server = http.createServer(function(req,res) {
 server.listen(3000,function(){
   console.log("The server is listening on port 3000");
 });
+
+
