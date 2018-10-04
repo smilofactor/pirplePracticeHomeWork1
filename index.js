63eb53de048c28a77d50d5a76cb41848d2933274
Steve M <smilovidov@gmail.com>
2018-10-04 07:55:12 -0400

    Added buffer capture/decode.Sec3,Vid8

--- index.js
+++ index.js
@@ -8,6 +8,7 @@
 //Dependencies
 var http = require('http');
 var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
 
 
 //The server should respond to all requests with a string
@@ -34,15 +35,33 @@ var server = http.createServer(function(req,res) {
   var method = req.method.toLowerCase();
 
 
-  //Send response
-  res.end('Outputting info\n');
+  //Get payload if any
+  var decoder = new StringDecoder('utf-8');
+  var buffer = '';
+  req.on('data',function(data){
+    buffer += decoder.write(data);
+  });
+
+  req.on('end',function(){
+    buffer += decoder.end();
+
+    //Send response
+    res.end('Outputting info\n');
+
+    console.log('Request received with this payload: ',buffer);
+
+  });
 
 
+
+  /*SVM
+   * Leaving this in to troubleshoot in the future
   //Log the request path
   console.log('\nQueryStringObject is: ',queryStringObject,
                 '\nrequest received on path: '+trimmedPath+ 
                 '\nrequest received with these headers',headers,
                 '\nusing this method: '+method);
+  */
  
 
 });
